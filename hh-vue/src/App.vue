ad37858bbcaa3b9bc64e861750df0a664af15900
hh1802 <462189066@.com>
2019-09-17 23:55:16 +0800

    12

---
+++ hh-vue/src/App.vue
@@ -0,0 +1,40 @@
+/* eslint-disable */
+// eslint-disable-next-line
+<template>
+  <div id="app">
+    <headerNav></headerNav>
+    <div class="content">
+      <router-view></router-view>
+      <!-- 这里是展示来自路由页面数据的 -->
+    </div>
+    <!--  引入的footer组件 -->
+    <footerNav></footerNav>
+
+    <img src="./assets/logo.png" />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import headerNav from '@/components/headerNav'
+import footerNav from '@/components/footerNav'
+export default {
+  name: 'App',
+  components: {
+    headerNav,
+    footerNav
+  },
+  mounted () {}
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ hh-vue/src/components/footerNav.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="footer">
+    <ul class="footer-con b2 ui-transition">
+      <li class="g1"></li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {}
+}
+</script>
+<style lang="css">
+.footer {
+  height: 2rem;
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  background-color: #fff;
+}
+.footer::before {
+  content: "";
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 1px;
+  background: #e0e0e0;
+}
+.footer-con li {
+  float: left;
+  width: 25%;
+  height: 2rem;
+  line-height: 2rem;
+  font-size: 0.8rem;
+  text-align: center;
+  -webkit-transition: ease 0.25s;
+  -moz-transition: ease 0.25s;
+  -ms-transition: ease 0.25s;
+  -o-transition: ease 0.25s;
+  transition: ease 0.25s;
+}
+.footer-con li:active {
+  background-color: rgb(248, 248, 248);
+}
+.footer-con:after {
+  content: "";
+  display: block;
+  clear: both;
+  width: 0;
+  height: 0;
+}
+</style>
